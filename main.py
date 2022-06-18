b932ba3b7043a234d37d86c336e70ded05ca7202
nguyenbaviet <nbviet98@gmail.com>
2022-06-18 14:40:00 +0700

    Update front end

--- main.py
+++ main.py
@@ -12,9 +12,5 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def get_hello(request: Request):
-  return templates.TemplateResponse("index.html", {"request": request, "id": "chao cu"})
-
-
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-  return templates.TemplateResponse("item.html", {"request": request, "id": id})
+  # return templates.TemplateResponse("index.html", {"request": request, "id": "chao cu"})
+  return templates.TemplateResponse("main.html", {"request": request})
--- static/js/index1.js
+++ static/js/index1.js
@@ -43,10 +43,8 @@ const recordButton = document.querySelector('#record');
 const pauseButton = document.querySelector('#pause')
 const stopButton = document.querySelector('#stop');
 const playButton = document.querySelector('#play');
-const saveButton = document.querySelector('#save');
-const savedAudioMessagesContainer = document.querySelector('#saved-audio-messages');
-const downloadMe = document.querySelector('#downloadMe');
 const s2tResult = document.querySelector('#s2tResult');
+const waitingS2T = document.querySelector('#waitingS2T');
 
 let recorder;
 let audio;
@@ -56,16 +54,15 @@ recordButton.addEventListener('click', async () => {
   pauseButton.removeAttribute('disabled');
   stopButton.removeAttribute('disabled');
   playButton.setAttribute('disabled', true);
-  saveButton.setAttribute('disabled', true);
-  downloadMe.style.display = 'None';
+  waitingS2T.style.display = 'None';
   if (!recorder) {
     recorder = await recordAudio();
   }
+  s2tResult.innerHTML = "";
   recorder.start();
 });
 
 pauseButton.addEventListener('click', async () => {
-  console.log(recorder.mediaRecorder.state);
   stopButton.removeAttribute('disabled');
   if(recorder.mediaRecorder.state === "recording") {
         recorder.pause();
@@ -83,43 +80,38 @@ stopButton.addEventListener('click', async () => {
   pauseButton.setAttribute('disabled', true);
   stopButton.setAttribute('disabled', true);
   playButton.removeAttribute('disabled');
-  saveButton.removeAttribute('disabled');
   pauseButton.textContent = "Pause";
   audio = await recorder.stop();
+
+  waitingS2T.style.display='block';
+  // display speech to text
+  const reader = new FileReader();
+   reader.readAsDataURL(audio.audioBlob);
+   reader.onload = () => {
+     const base64AudioMessage = reader.result.split(',')[1];
+     const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      const raw = JSON.stringify({
+        "base_64_voice": base64AudioMessage
+      });
+
+      const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+     fetch("https://tbot1.anhph.com/s2t", requestOptions)
+    .then(response => response.text())
+    .then(result => {
+        waitingS2T.style.display="None";
+        s2tResult.innerHTML = JSON.parse(JSON.parse(result).result).transcript;
+    })
+    .catch(error => console.log('error', error));
+    };
 });
 
 playButton.addEventListener('click', () => {
-  console.log(audio.duration);
   audio.play();
-});
-
-// saveButton.addEventListener('click', () => {
-//   downloadMe.href = audio.audioUrl;
-//   downloadMe.download = 'v_test.wav';
-//   downloadMe.style.display="block";
-// });
-
-saveButton.addEventListener('click', () => {
- const reader = new FileReader();
- reader.readAsDataURL(audio.audioBlob);
- reader.onload = () => {
-   const base64AudioMessage = reader.result.split(',')[1];
-   const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-
-    const raw = JSON.stringify({
-      "base_64_voice": base64AudioMessage
-    });
-
-    const requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
-    };
-   fetch("https://tbot1.anhph.com/s2t", requestOptions)
-  .then(response => response.text())
-  .then(result => s2tResult.textContent = result)
-  .catch(error => console.log('error', error));
- };
 });
