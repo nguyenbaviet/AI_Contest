aa54ddaaba197a03a5039c60ff67e7423bb9fe7d
nguyenbaviet <nbviet98@gmail.com>
2022-06-18 21:21:06 +0700

    voice enroll + voice verify

--- static/js/index1.js
+++ static/js/index1.js
@@ -37,6 +37,19 @@ const recordAudio = () =>
     resolve({ start, stop, pause, resume, mediaRecorder});
   });
 
+function convertVoiceToB64(){
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
+    let b64;
+    reader.readAsDataURL(audio.audioBlob);
+    reader.onload = () => {
+      b64 = reader.result.split(',')[1];
+      resolve(b64);
+    };
+  }, 5000)
+}
+
+
 const sleep = time => new Promise(resolve => setTimeout(resolve, time));
 
 const recordButton = document.querySelector('#record');
@@ -45,15 +58,104 @@ const stopButton = document.querySelector('#stop');
 const playButton = document.querySelector('#play');
 const s2tResult = document.querySelector('#s2tResult');
 const waitingS2T = document.querySelector('#waitingS2T');
+const s2tResultEnroll = document.querySelector('#s2tResultEnroll');
+const waitingS2TEnroll = document.querySelector('#waitingS2TEnroll');
+const btnVoicePayment = document.querySelector('.btnVoicePayment');
+const btnEnrollVoice = document.querySelector('.btnEnrollVoice');
+const btnVoice = document.querySelector('#btnVoice');
+const btnEnroll = document.querySelector('#btnEnroll');
+
 
 let recorder;
 let audio;
+let purposeText;
+let token_id = '';
+let base64AudioMessage;
+
+// switch to payment modal
+btnVoicePayment.addEventListener('click', async () => {
+  purposeText = 'voice'
+  btnVoice.style.display = 'block';
+  btnEnroll.style.display = 'none';
+  waitingS2T.style.display = 'none';
+  btnEnroll.setAttribute('disabled', true);
+  btnVoice.setAttribute('disabled', true);
+  s2tResult.innerHTML = "";
+  s2tResultEnroll.innerHTML = "";
+});
+
+// switch to enroll modal
+btnEnrollVoice.addEventListener('click', async () => {
+  purposeText = 'enroll'
+  btnEnroll.style.display = 'block';
+  btnVoice.style.display = 'none';
+  waitingS2T.style.display = 'none';
+  btnEnroll.setAttribute('disabled', true);
+  btnVoice.setAttribute('disabled', true);
+  s2tResultEnroll.innerHTML = "";
+});
+
+// enroll voice to get voice id
+btnEnroll.addEventListener('click', async () => {
+  waitingS2TEnroll.style.display = "block";
+  s2tResultEnroll.innerHTML = "";
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify({
+    "base_64_voice": base64AudioMessage,
+    "token": token_id
+  });
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+
+  fetch("https://tbot1.anhph.com/enroll", requestOptions)
+    .then(response => response.text())
+    .then(result => {
+      console.log(result);
+      console.log("style: ", waitingS2TEnroll.style.display);
+      waitingS2TEnroll.style.display = "none";
+      console.log("style: ", waitingS2TEnroll.style.display);
+      var content = JSON.parse(JSON.parse(result).result).content;
+      console.log(content);
+      if (content == 'Success'){
+          s2tResultEnroll.innerHTML = '<div style="color: blue"> Enroll voice successfully.</div>'
+          var payload = JSON.parse(JSON.parse(result).result).payload;
+          var voice_id = payload.id;
+          requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: JSON.stringify({
+                    "item": voice_id
+                  }),
+            redirect: 'follow'
+          };
+          fetch("/save/" + voice_id, requestOptions)
+          .then(response => response.json())
+              .then(result => {
+                  console.log("result: ", result);
+              })
+              .catch(error => console.log('error', error));
+      } else {
+        s2tResultEnroll.innerHTML = '<div style="color: red"> Failed to enroll your voice. Please speak more longer or check your connection!!!</div>';
+      }
+    })
+    .catch(error => console.log('error', error));
+});
+
 
 recordButton.addEventListener('click', async () => {
   recordButton.setAttribute('disabled', true);
   pauseButton.removeAttribute('disabled');
   stopButton.removeAttribute('disabled');
   playButton.setAttribute('disabled', true);
+  btnEnroll.setAttribute('disabled', true);
+  btnVoice.setAttribute('disabled', true);
   waitingS2T.style.display = 'None';
   if (!recorder) {
     recorder = await recordAudio();
@@ -84,11 +186,34 @@ stopButton.addEventListener('click', async () => {
   audio = await recorder.stop();
 
   waitingS2T.style.display='block';
-  // display speech to text
-  const reader = new FileReader();
-   reader.readAsDataURL(audio.audioBlob);
-   reader.onload = () => {
-     const base64AudioMessage = reader.result.split(',')[1];
+
+  // get token id
+    var raw = "";
+    console.log("before token: ", token_id);
+    if (token_id == ''){
+      var requestOptions = {
+          method: 'POST',
+          body: raw,
+          redirect: 'follow'
+        };
+
+        token_id = await new Promise((resolve, reject) => {
+          let t_id = '';
+          fetch("https://tbot1.anhph.com/login", requestOptions)
+                  .then(response => response.text())
+                  .then(result => {
+                    t_id = JSON.parse(JSON.parse(result).result).token;
+                    resolve(t_id);
+                  })
+                  .catch(error => console.log('error', error));
+        }, 5000)
+        }
+    console.log("after token: ", token_id);
+  // encode voice to base64
+  base64AudioMessage = await convertVoiceToB64();
+
+  // Speech to text
+  if (purposeText == 'voice'){
      const myHeaders = new Headers();
       myHeaders.append("Content-Type", "application/json");
 
@@ -105,11 +230,48 @@ stopButton.addEventListener('click', async () => {
      fetch("https://tbot1.anhph.com/s2t", requestOptions)
     .then(response => response.text())
     .then(result => {
-        waitingS2T.style.display="None";
-        s2tResult.innerHTML = JSON.parse(JSON.parse(result).result).transcript;
+          console.log("transcript: ", JSON.parse(JSON.parse(result).result).transcript);
+//        waitingS2T.style.display="None";
+//        s2tResult.innerHTML = JSON.parse(JSON.parse(result).result).transcript;
+//        btnVoice.removeAttribute("disabled");
+
+        // get voice id
+        fetch("/get_voice_id")
+        .then(response => response.text())
+        .then(voice_id => {
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+            console.log("token: ", token_id);
+            console.log("voice: ", voice_id);
+            var raw = JSON.stringify({
+              "base_64_voice": base64AudioMessage,
+              "token": token_id,
+              "voice_id": voice_id
+            });
+
+            var requestOptions = {
+              method: 'POST',
+              headers: myHeaders,
+              body: raw,
+              redirect: 'follow'
+            };
+            fetch("https://tbot1.anhph.com/verify", requestOptions)
+              .then(response => response.text())
+              .then(result => {
+                waitingS2T.style.display="None";
+                s2tResult.innerHTML = JSON.parse(JSON.parse(result).result).detail;
+                btnVoice.removeAttribute("disabled");
+              })
+              .catch(error => console.log('error', error));
+        })
+        .catch(error => console.log('error', error));
     })
     .catch(error => console.log('error', error));
-    };
+  }else{
+    waitingS2T.style.display="None";
+    s2tResult.innerHTML = '<div style="color: blue"> Ready to enroll your voice. Please click <i>Enroll</i> button to finish.</div>';
+    btnEnroll.removeAttribute("disabled");
+  }
 });
 
 playButton.addEventListener('click', () => {
